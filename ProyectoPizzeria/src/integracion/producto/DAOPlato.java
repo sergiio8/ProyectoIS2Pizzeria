0d29bc2a69bb2773ac43de424bf89fb9e9561c7a
danie <danie@XPSDanielCP4>
2023-04-22 14:27:13 +0200

    como estan los maquinas

--- ProyectoPizzeria/src/integracion/producto/DAOPlato.java
+++ ProyectoPizzeria/src/integracion/producto/DAOPlato.java
@@ -1,5 +1,11 @@
 package integracion.producto;
 
+import negocio.producto.TPlato;
+
 public interface DAOPlato {
 	
+	public String insertaMesa(TPlato tp);
+	public Boolean daDeBajaPlato(String id);
+	public TPlato obtenPlato(String id);
+	Boolean modificaPlato(TPlato tp);
 }
