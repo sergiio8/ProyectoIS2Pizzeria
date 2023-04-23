e27b90fc27052cb31eedca890cc3c2265bd18318
danie <danie@LAPTOP-4PPNFQ3D>
2023-04-23 20:18:28 +0200

    Añadido el stock en TPlato

--- ProyectoPizzeria/src/negocio/producto/TEntrante.java
+++ ProyectoPizzeria/src/negocio/producto/TEntrante.java
@@ -9,8 +9,8 @@ public class TEntrante extends TPlato{
 	private Salsa salsa;
 	
 	public TEntrante(String id, String nombre, double precio, ArrayList<TIngrediente> ingredientes,
-			String descripcion, Salsa salsa) {
-		super(id, nombre, precio, ingredientes, descripcion);
+			String descripcion, int stock, Salsa salsa) {
+		super(id, nombre, precio, ingredientes, descripcion, stock);
 		this.salsa = salsa;
 	}
 	
